47624db9b8800a2530bfb0bd9a95837bb7d07d8e
Roland Branten <roland.branten@gmail.com>
2020-04-01 22:13:37 +0200

    different test

--- sw-range-request-handler.js
+++ sw-range-request-handler.js
@@ -2,10 +2,8 @@ workbox.routing.registerRoute(
   /.*\.mp4/,
   new workbox.strategies.CacheFirst({
     plugins: [
-      new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [200]
-      }),
-      new workbox.rangeRequests.RangeRequestsPlugin()
+      new workbox.cacheableResponse.Plugin({ statuses: [200] }),
+      new workbox.rangeRequests.Plugin()
     ]
   }),
   "GET"
