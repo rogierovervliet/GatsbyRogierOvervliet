83509e93598a862529929797f20f8190a960083f
Roland Branten <roland.branten@gmail.com>
2020-04-01 18:12:45 +0200

    ios test 2

--- src/macro-home/Video.js
+++ src/macro-home/Video.js
@@ -117,7 +117,7 @@ export default function Video() {
       }}
       onClick={mute}
     >
-      <VideoComp controls autoPlay muted loop playsInline ref={videoRef}>
+      <VideoComp autoPlay muted loop playsInline ref={videoRef}>
         <source src={mp4} type="video/mp4" />
       </VideoComp>
       <Mute>
