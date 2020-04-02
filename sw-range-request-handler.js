16bf6b5efe764bf7d849c0764788a2ab9fc4aa0e
Roland Branten <roland.branten@gmail.com>
2020-04-02 08:15:09 +0200

    safari test

--- sw-range-request-handler.js
+++ sw-range-request-handler.js
@@ -1,7 +1,11 @@
-workbox.routing.registerRoute(
-  /.*\.(mp4|webm)/,
-  workbox.strategies.cacheFirst({
-    plugins: [new workbox.rangeRequests.Plugin()]
-  }),
-  "GET"
-);
+let is_safari = navigator.userAgent.indexOf("Safari") > -1;
+
+if (is_safari) {
+  workbox.routing.registerRoute(
+    /.*\.(mp4|webm)/,
+    workbox.strategies.cacheFirst({
+      plugins: [new workbox.rangeRequests.Plugin()]
+    }),
+    "GET"
+  );
+}
