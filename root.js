fc922de704a92f24a5d3701e1a97c2859def2af5
Roland Branten <roland.branten@gmail.com>
2020-04-02 06:58:52 +0200

    service worker test

--- root.js
+++ root.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Overflow = styled.div`
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   overflow-x: hidden;
 `;
 
--- sw-range-request-handler.js
+++ sw-range-request-handler.js
@@ -1,10 +1,5 @@
 workbox.routing.registerRoute(
-  /.*\.mp4/,
-  new workbox.strategies.CacheFirst({
-    plugins: [
-      new workbox.cacheableResponse.Plugin({ statuses: [200] }),
-      new workbox.rangeRequests.Plugin()
-    ]
-  }),
+  /^https?:.*\.mp4$/,
+  new workbox.strategies.StaleWhileRevalidate(),
   "GET"
 );
